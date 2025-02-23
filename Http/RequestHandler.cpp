7d0642d7fc5067b9e85b7706888d6e266ec400a1
aes-sarg <aes-sarg@e2r8p5.1337.ma>
2025-02-23 23:47:52 +0000

    Fix match location issue one more time

--- Http/RequestHandler.cpp
+++ Http/RequestHandler.cpp
@@ -6,7 +6,7 @@
 /*   By: aes-sarg <aes-sarg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 20:43:44 by aes-sarg          #+#    #+#             */
-/*   Updated: 2025/02/23 23:25:20 by aes-sarg         ###   ########.fr       */
+/*   Updated: 2025/02/23 23:47:04 by aes-sarg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -218,6 +218,7 @@ void RequestHandler::handleRequest(int client_sockfd, string req, int bytes_rece
                 LocationConfig location;
                 if (this->matchLocation(location, request.getDecodedPath(), request))
                 {
+
                     if (!ServerUtils::isMethodAllowed(request.getMethod(), location.getMethods()))
                         throw NOT_ALLOWED;
                     if (location.getUploadDir().empty())
@@ -227,8 +228,9 @@ void RequestHandler::handleRequest(int client_sockfd, string req, int bytes_rece
                     state.headers_parsed = true;
                     state.content_remaining = 0;
 
-                    state.upload_path = location.getRoot() + request.getDecodedPath() + location.getUploadDir() + ServerUtils::generateUniqueString() +
+                    state.upload_path = location.getRoot() + request.getDecodedPath() + location.getUploadDir() + "/" + ServerUtils::generateUniqueString() +
                                         ServerUtils::getFileExtention(request.getHeader(CONTENT_TYPE));
+
                     state.output_file.open(state.upload_path.c_str(), std::ios::binary);
 
                     if (!state.output_file.is_open())
@@ -249,7 +251,6 @@ void RequestHandler::handleRequest(int client_sockfd, string req, int bytes_rece
 
                 if (this->matchLocation(location, request.getDecodedPath(), request))
                 {
-                    cout << "MATCHED LOCATION: " << location.getPath() << endl;
                     if (!ServerUtils::isMethodAllowed(request.getMethod(), location.getMethods()))
                         throw NOT_ALLOWED;
                     if (is_CgiRequest(url, location.getCgiExtension()))
@@ -264,7 +265,6 @@ void RequestHandler::handleRequest(int client_sockfd, string req, int bytes_rece
                         modifyEpollEvent(epoll_fd, client_sockfd, EPOLLOUT);
                         return;
                     }
-                    cout << "upload dir " << location.getUploadDir() << endl;
                     if (location.getUploadDir().empty())
                         throw UNAUTHORIZED;
                     ChunkedUploadState state;
@@ -274,7 +274,6 @@ void RequestHandler::handleRequest(int client_sockfd, string req, int bytes_rece
 
                     state.upload_path = location.getRoot() + request.getDecodedPath() + location.getUploadDir() + "/" + ServerUtils::generateUniqueString() +
                                         ServerUtils::getFileExtention(request.getHeader(CONTENT_TYPE));
-                    cout << "UPLOAD PATH : " << state.upload_path << endl;
                     state.output_file.open(state.upload_path.c_str(), std::ios::binary);
 
                     if (!state.output_file.is_open())
@@ -690,14 +689,13 @@ bool RequestHandler::matchLocation(LocationConfig &loc, const string url, const 
     {
         const string &path = locs[i].getPath();
 
-        if (url.find(path) == 0)
+        if (url.find(path) != string::npos)
         {
             size_t pathLength = path.length();
             if (pathLength > bestMatchLength)
             {
                 char nextChar = url[pathLength];
-                cout << "NEXT CHAR: " << nextChar << endl;
-                if (nextChar == '/' || path[pathLength] == '\0')
+                if (nextChar == '/' || path == "/")
                 {
                     found = true;
                     bestMatch = locs[i];
